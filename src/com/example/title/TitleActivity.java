f83eacb544e7d57a9f13bc43165ec652653c2d07
1156488534 <wsr1156488534@163.com>
2013-07-05 08:50:21 +0800

    F

---
+++ src/com/example/title/TitleActivity.java
@@ -0,0 +1,24 @@
+package com.example.title;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.Window;
+
+public class TitleActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_title);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.title, menu);
+		return true;
+	}
+
+}
